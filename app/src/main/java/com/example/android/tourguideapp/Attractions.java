3dd6e35a3a1b929697a8462c7e7989e5b66075f9
Don Mears <Donaldmears1@yahoo.com>
2018-07-05 17:21:59 -0400

    Added Attractions.java custom class.

---
+++ app/src/main/java/com/example/android/tourguideapp/Attractions.java
@@ -0,0 +1,80 @@
+package com.example.android.tourguideapp;
+
+/**
+ * Attractions class to maintain all attractions data
+ *
+ * @Author: Donald Mears
+ * @date: 7/5/2018
+ */
+public class Attractions {
+
+    private String attractName;
+
+    private String attractAddress;
+
+    private String attractPhoNumber;
+
+    private int attractImageResourceId;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    /**
+     * 2nd Constructor
+     *
+     * @param name            Attraction name
+     * @param address         Attraction address
+     * @param phoneNumber     Attraction phone number
+     * @param imageResourceId Attraction image
+     */
+    public Attractions(String name, String address, String phoneNumber, int imageResourceId) {
+        attractName = name;
+        attractAddress = address;
+        attractPhoNumber = phoneNumber;
+        attractImageResourceId = imageResourceId;
+    }
+
+    /**
+     * method to return attraction name
+     *
+     * @return attractName   Name of Attraction
+     */
+    public String getName() {
+        return attractName;
+    }
+
+    /**
+     * method to return attraction address
+     *
+     * @return attractAddress   Address of Attraction
+     */
+    public String getAddress() {
+        return attractAddress;
+    }
+
+    /**
+     * method to return attraction phone number
+     *
+     * @return attractPhoNumber   Phone Number of Attraction
+     */
+    public String getPhoneNumber() {
+        return attractPhoNumber;
+    }
+
+    /**
+     * method to return image
+     *
+     * @return attractImageResource   Image of Attraction
+     */
+    public int getImageResourceId() {
+        return attractImageResourceId;
+    }
+
+    /**
+     * method to check if object has image
+     *
+     * @return attractImageResourceId  returns true or false
+     */
+    public boolean hasImage() {
+        return attractImageResourceId != NO_IMAGE_PROVIDED;
+    }
+}
